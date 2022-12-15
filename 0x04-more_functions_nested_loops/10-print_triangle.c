6dfaca7e10e1474948affb8ed102123b7b0d4d5f
engy <engyalaa.ea.77@gmail.com>
2022-12-15 11:38:19 -0800

    C - More functions, more nested

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,11 +12,18 @@ void print_triangle(int size)
 	for (i = 0; i < size; i++)
 	{
 		for (j = 1; j < (size - i); j++)
+		{
 			_putchar(' ');
+		}
 		for (j--; j < size; j++)
+		{
 			_putchar(35);
+		}
 		if (i < (size - 1))
+		{
 			_putchar('\n');
+
+		}
 	}
 	_putchar('\n');
 }
