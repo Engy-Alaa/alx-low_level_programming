fffd0c4ff1dd660f83bb6f3a8a750fa1424def46
engy <engyalaa.ea.77@gmail.com>
2022-12-15 09:10:00 -0800

    C - More functions, more nested

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,7 +10,7 @@ void print_most_numbers(void)
 
 	for (number = 48; number < 58; number++)
 	{
-		if ((number != 50) || (number != 52))
+		if ((number == 50) || (number == 52))
 		{
 			continue;
 		}
