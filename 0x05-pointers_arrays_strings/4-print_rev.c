14207265757c29888ce6ae882711f65c4d4da714
engy <engyalaa.ea.77@gmail.com>
2022-12-19 09:12:47 -0800

    C-Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,15 +4,16 @@
 * @s: string to print
 */
 
-void print_rev(char *str)
+void print_rev(char *s)
 {
 	int count, inc;
 
 	inc = 0;
-	for (count = 0; str[count] != '\0'; count++)
+	for (count = 0; s[count] != '\0'; count++)
 		inc++;
 
 	for (count = inc; count >= 0; count--)
-		_putchar(str[count]);
+		_putchar(s[count]);
+	_putchar(10);
 	return (0);
 }
