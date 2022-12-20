a6d8198bf38686767d99c0ea1188caabe66d7edb
engy <engyalaa.ea.77@gmail.com>
2022-12-19 16:06:23 -0800

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,11 +8,13 @@
  **/
 void puts2(char *str)
 {
-	int i;
+int i = 0;
+while (str[i] != '\0')
+{
+if (i % 2 == 0)
+_putchar(str[i]);
+i++;
+}
 
-	for (i = 0; str[i] != ' \0'; i = i + 2)
-	{
-		_putchar(str[i]);
-	}
-	_putchar('\n');
+_putchar('\n');
 }
