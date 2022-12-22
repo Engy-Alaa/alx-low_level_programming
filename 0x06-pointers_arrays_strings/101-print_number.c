16fdddbb3d6b2a9e8a245f26b981be29e7fed65a
engy <engyalaa.ea.77@gmail.com>
2022-12-22 15:16:49 -0800

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,17 +1,17 @@
 #include "main.h"
-
 /**
  * print_number - prints an integer.
  * @n: input integer.
  * Return: no return.
  */
+
 void print_number(int n)
 {
 	unsigned int m, d, count;
 
 	if (n < 0)
 	{
-		putchar(45);
+		_putchar(45);
 		m = n * -1;
 	}
 	else
@@ -30,6 +30,6 @@ void print_number(int n)
 
 	for (; count >= 1; count /= 10)
 	{
-		putchar(((m / count) % 10) + 48);
+		_putchar(((m / count) % 10) + 48);
 	}
 }
