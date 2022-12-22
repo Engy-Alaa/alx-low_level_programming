b0ed111ada63091f54fc9d5f365f14caa57c16af
engy <engyalaa.ea.77@gmail.com>
2022-12-22 14:30:18 -0800

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,8 +1,10 @@
-#include"main.h"
+#include "main.h"
+
 /**
  * print_number - Prints an integer.
  * @n: The integer to be printed.
  */
+
 void print_number(int n)
 {
 	unsigned int num = n;
@@ -12,9 +14,9 @@ void print_number(int n)
 		_putchar('-');
 		num = -num;
 	}
-	
+
 	if ((num / 10) > 0)
 		print_number(num / 10);
-	
+
 	_putchar((num % 10) + '0');
 }
