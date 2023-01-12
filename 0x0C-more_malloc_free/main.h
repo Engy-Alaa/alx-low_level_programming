9a6c7066c1557ad85fd5edb1b3f3c02163b8033a
engy <engyalaa.ea.77@gmail.com>
2023-01-12 13:32:14 -0800

    C- More malloc, free

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -13,11 +13,6 @@ int len(char *str);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int find_len(char *str);
-char *create_xarray(int size);
-char *iterate_zeroes(char *str);
-void get_prod(char *prod, char *mult, int digit, int zeroes);
-void add_nums(char *final_prod, char *next_prod, int next_len);
 
 #endif /*MAIN_H*/
 
