c100a307c92dc7a6b7ba745a1ce65713e66c2595
engy <engyalaa.ea.77@gmail.com>
2023-01-16 17:52:53 -0800

    C - Structures, typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -31,7 +31,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dogt->name = malloc(lname + 1);
 	dogt->owner = malloc(lowner + 1);
 
-	if (!(p_dog->name) || !(p_dog->owner))
+	if (!(dogt->name) || !(dogt->owner))
 	{
 		free(dogt->owner);
 		free(dogt->name);
