ce26e8b0edb85adb01015a4a606bc25485e786e1
engy <engyalaa.ea.77@gmail.com>
2023-01-16 16:53:30 -0800

    C - Structures, typedef

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,38 @@
+#include "dog.h"
+
+/**
+* print_dog - prints a struct dog
+*@d: dog identification
+*
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+		{
+			printf("Name: %s\n", d->name);
+		}
+		else
+		{
+			printf("nil\n");
+		}
+		if (d->age > 0)
+		{
+			printf("Age: %f\n", d->age);
+		}
+		else
+		{
+			printf("nil\n");
+		}
+		if (d->owner != NULL)
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+		else
+		{
+			printf("nil\n");
+		}
+	}
+}
