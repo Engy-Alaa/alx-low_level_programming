c47d6fc4c9d417277d963d239e9816a70df1f5e2
engy <engyalaa.ea.77@gmail.com>
2023-01-16 17:14:56 -0800

    C - Structures, typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,39 @@
 #include "dog.h"
+
+/**
+*_strlen - returns length of
+*a string
+*@str: string to be counted
+*Return: returns length of string
+*/
+int _strlen(char *str)
+{
+int len = 0;
+while (str)
+len++;
+
+return (len);
+}
+
+
+/**
+*_strcopy - copy string pointed by src
+*into dest variable
+*@dest:buffer storing string copy
+*@src: buffer storing string to copy
+*Return:returns copied string
+*/
+char *_strcpy(char *dest, char *src)
+{
+int index = 0;
+
+for (; src[index] ; index++)
+dest[index] = src[index];
+
+dest[index] = '\0';
+return (dest);
+}
+
 /**
 *new_dog - creates a new dog
 *@name: name of new dog
@@ -17,8 +52,8 @@ new_dog = malloc(sizeof(dog_t));
 if (new_dog == NULL)
 	return (NULL);
 
-new_dog->name = malloc(sizeof(char) * (strlen(owner) + 1));
-new_dog->name = strcpy(new_dog->name, name);
+new_dog->name = malloc(sizeof(char) * (_strlen(owner) + 1));
+new_dog->name = _strcpy(new_dog->name, name);
 
 if (new_dog->name == NULL)
 	{
@@ -26,8 +61,8 @@ if (new_dog->name == NULL)
 	return (NULL);
 	}
 
-new_dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
-new_dog->owner = strcpy(new_dog->owner, owner);
+new_dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+new_dog->owner = _strcpy(new_dog->owner, owner);
 
 if (new_dog->owner == NULL)
 	{
