14db8465c71634ec87c589a5d999260a34f288e0
Engy <engyalaa.ea.77@gmail.com>
2023-03-17 01:04:39 +0200

    C - Doubly linked lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,46 +1,53 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - insert node at specific index
- * @h: head of linked list
- * @idx: index of new node
- * @n: new node value
- * Return: inserted node
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: pointer to the list
+ * @idx: index of the new node
+ * @n: value of the new node
+ * Return: address of the new node, or NULL if it fails
  */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current;
-	dlistint_t *new;
+dlistint_t *new;
+dlistint_t *tmp;
+unsigned int i;
 
-	if (h == NULL)
-		return (0);
-
-	current = *h;
-
-	while (idx != 0)
-	{
-		current = current->next;
-		idx--;
-		if (current == NULL)
-			return (NULL);
-	}
-
-	new = malloc(sizeof(dlistint_t));
-
-	if (new == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
-
-	new->n = n;
-	new->next = current;
-	new->prev = current->prev;
-	if (current->prev != NULL)
-		current->prev->next = new;
-
-	/*TODO: Handle special case when idx is 0 and last index*/
-
-	return (current);
+if (h == NULL)
+return (NULL);
+tmp = *h;
+i = 0;
+if (idx == 0)
+{
+new = malloc(sizeof(dlistint_t));
+if (new == NULL)
+return (NULL);
+new->n = n;
+new->next = *h;
+new->prev = NULL;
+if (*h != NULL)
+(*h)->prev = new;
+*h = new;
+return (new);
+}
+while (tmp != NULL)
+{
+if (i == idx - 1)
+{
+new = malloc(sizeof(dlistint_t));
+if (new == NULL)
+return (NULL);
+new->n = n;
+new->next = tmp->next;
+new->prev = tmp;
+tmp->next = new;
+if (new->next != NULL)
+new->next->prev = new;
+return (new);
+}
+tmp = tmp->next;
+i++;
+}
+return (NULL);
 }
