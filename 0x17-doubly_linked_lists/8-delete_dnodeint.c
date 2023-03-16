74924a31aebcf1cf7f569505b45ee0840414d40b
Engy <engyalaa.ea.77@gmail.com>
2023-03-17 01:13:37 +0200

    C - Doubly linked lists

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,47 +1,52 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index- deletes the node at a given index
- * @head: pointer to the list
- * @index: index of the node to delete
+ * delete_dnodeint_at_index - deletes the node at index of a
+ * dlistint_t linked list
+ *
+ * @head: head of the list
+ * @index: index of the new node
  * Return: 1 if it succeeded, -1 if it failed
-*/
+ */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-dlistint_t *tmp;
-dlistint_t *tmp2;
-unsigned int i;
+	dlistint_t *h1;
+	dlistint_t *h2;
+	unsigned int i;
 
-if (head == NULL)
-return (-1);
-if (*head == NULL)
-return (-1);
-if (index == 0)
-{
-tmp = *head;
-*head = (*head)->next;
-if (*head != NULL)
-(*head)->prev = NULL;
-free(tmp);
-return (1);
-}
-tmp = *head;
-i = 0;
-while (tmp != NULL)
-{
-if (i == index - 1)
-{
-tmp2 = tmp->next;
-tmp->next = tmp->next->next;
-if (tmp->next != NULL)
-tmp->next->prev = tmp;
-free(tmp2);
-return (1);
-}
-tmp = tmp->next;
-i++;
-}
-return (-1);
+	h1 = *head;
 
-return (1);
+	if (h1 != NULL)
+		while (h1->prev != NULL)
+			h1 = h1->prev;
+
+	i = 0;
+
+	while (h1 != NULL)
+	{
+		if (i == index)
+		{
+			if (i == 0)
+			{
+				*head = h1->next;
+				if (*head != NULL)
+					(*head)->prev = NULL;
+			}
+			else
+			{
+				h2->next = h1->next;
+
+				if (h1->next != NULL)
+					h1->next->prev = h2;
+			}
+
+			free(h1);
+			return (1);
+		}
+		h2 = h1;
+		h1 = h1->next;
+		i++;
+	}
+
+	return (-1);
 }
